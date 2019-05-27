6f26d48f9e02ac4e2dc2fce8087ce91ab36f3b37
Liz <jesusridesthebus@gmail.com>
2019-05-26 22:13:43 -0700

    add tables to database

--- liz_kelley.sql
+++ liz_kelley.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 22, 2019 at 06:34 AM
+-- Generation Time: May 27, 2019 at 04:40 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.10
 
@@ -41,7 +41,17 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`id`, `name`, `stylist_id`) VALUES
-(1, 'face', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialties`
+--
+
+CREATE TABLE `specialties` (
+  `id` int(11) NOT NULL,
+  `specialty` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -59,7 +69,9 @@ CREATE TABLE `stylists` (
 --
 
 INSERT INTO `stylists` (`id`, `name`) VALUES
-(1, 'fart');
+(4, 'billy'),
+(5, 'poopfart'),
+(6, 'peeface');
 
 -- --------------------------------------------------------
 
@@ -74,6 +86,24 @@ CREATE TABLE `stylist_client` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `stylist_client`
+--
+
+INSERT INTO `stylist_client` (`id`, `stylist_id`, `client_id`) VALUES
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stylist_specialty`
+--
+
+CREATE TABLE `stylist_specialty` (
+  `id` int(11) NOT NULL,
+  `stylist_id` int(11) NOT NULL,
+  `specialty_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
 -- Indexes for dumped tables
 --
 
@@ -81,6 +111,12 @@ CREATE TABLE `stylist_client` (
 -- Indexes for table `clients`
 --
 ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `specialties`
+--
+ALTER TABLE `specialties`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -96,6 +132,12 @@ ALTER TABLE `stylist_client`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `stylist_specialty`
+--
+ALTER TABLE `stylist_specialty`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -103,18 +145,30 @@ ALTER TABLE `stylist_client`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `specialties`
+--
+ALTER TABLE `specialties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `stylists`
 --
 ALTER TABLE `stylists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `stylist_client`
 --
 ALTER TABLE `stylist_client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `stylist_specialty`
+--
+ALTER TABLE `stylist_specialty`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
